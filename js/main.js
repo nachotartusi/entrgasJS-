73b31bb5b7bc34f1b86bd88582eabb410d50f02b
nacho tartusi <nachotartusi@gmail.com>
2022-07-18 19:47:31 -0300

    entrega 2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ class Persona {
         this.telefono = telefono;
         this.tipoConsulta= tipoConsulta;
         this.nroCliente=nroCliente;
-    } 
+        this.impreso=false;
+    }
 } 
 //array
 const arrayPersonas = [];
@@ -22,37 +23,42 @@ function cargarPaciente() {
     arrayPersonas.push(paciente);
     console.log(paciente);
     guardarPaciente(arrayPersonas);
-    listaPacienteDOM ();
+    listaPacienteDOM (JSON.parse(localStorage.getItem("paciente")));
 }
 
-function listaPacienteDOM (arrayPersonas) {
+function listaPacienteDOM (arrayLista) {
+    console.log(arrayLista);
     let formulario = document.getElementById ("form");
     formulario.reset();
-    let pacientes = obtenerPaciente();
 
-    for (let arrayPersonas of pacientes) {
-        let nuevoDiv = document.getElementById("main");
-        let nuevoPaciente = document.createElement ("html");
-        let html = `
-        <div class="card d-flex flex-row" style="width: 18rem>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Nombre </li>
-                <li class="list-group-item">${arrayPersonas.nombre}</li>
-                <li class="list-group-item">Edad</li>
-                <li class="list-group-item">${arrayPersonas.edad} </li>
-                <li class="list-group-item">Telefono</li>
-                <li class="list-group-item">${arrayPersonas.telefono} </li>
-                <li class="list-group-item"> Precio </li>
-                <li class="list-group-item"> ${arrayPersonas.nroCliente} </li>
-                <li class="list-group-item"> Paciente Numero </li>
-                <li class="list-group-item">${arrayPersonas.tipoConsulta} </li>
-                <button id="botonEliminar"> Eliminar Paciente </button>
-                
-            </ul>
-        </div>`;
-    nuevoPaciente.innerHTML = html;
-    nuevoPaciente.className = "card1";
-    nuevoDiv.appendChild(nuevoPaciente); }
+    for (let i=0; i < arrayLista.length; i++) {
+        console.log(arrayLista[i].impreso);
+        if(arrayLista[i].impreso == false){
+            let nuevoDiv = document.getElementById("main");
+            let nuevoPaciente = document.createElement ("html");
+            let html = `
+            <div class="card d-flex flex-row" style="width: 18rem>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Nombre </li>
+                    <li class="list-group-item">${arrayLista[i].nombre}</li>
+                    <li class="list-group-item">Edad</li>
+                    <li class="list-group-item">${arrayLista[i].edad} </li>
+                    <li class="list-group-item">Telefono</li>
+                    <li class="list-group-item">${arrayLista[i].telefono} </li>
+                    <li class="list-group-item"> Precio </li>
+                    <li class="list-group-item"> ${arrayLista[i].nroCliente} </li>
+                    <li class="list-group-item"> Paciente Numero </li>
+                    <li class="list-group-item">${arrayLista[i].tipoConsulta} </li>
+                    <button id="botonEliminar"> Eliminar Paciente </button>
+                    
+                </ul>
+            </div>`;
+            nuevoPaciente.innerHTML = html;
+            nuevoPaciente.className = "card1";
+            nuevoDiv.append(nuevoPaciente);
+            arrayPersonas[i].impreso=true;
+        }
+    }
     
     
     
