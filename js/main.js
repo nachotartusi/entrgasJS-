7a9c4119468f5faa78caeca657eac0f5b5916d98
nacho tartusi <nachotartusi@gmail.com>
2022-08-01 19:12:29 -0300

    entrega fetch

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 class Persona {
-    constructor (nombre,edad,telefono,tipoConsulta,nroCliente) {
+    constructor (nombre,edad,telefono,tipoConsulta,nroCliente,mail) {
         this.nombre = nombre.toUpperCase();
         this.edad = edad;
         this.telefono = telefono;
-        this.tipoConsulta= tipoConsulta;
-        this.nroCliente=nroCliente;
+        this.tipoConsulta = tipoConsulta;
+        this.nroCliente = nroCliente;
+        this.mail = mail;
     }
 } 
 
@@ -17,27 +18,21 @@ function setPacientes(){
 function listaPacienteDOM (arrayLista) {
     let formulario = document.getElementById ("form");
     formulario.reset();
-    let html1 = `
-    <div id="main2" class="card" style="width: 18rem>
-    </div>`;
+    let html1 = `<div id="main2"></div>`;
     document.getElementById("main2").innerHTML = html1; 
     if(arrayLista!=null){
         for (let i=0; i < arrayLista.length; i++){
             let nuevoDiv = document.getElementById("main2");
             let nuevoPaciente = document.createElement ("div");
             let html = `
-            <div class="card d-flex flex-row " id="" style="width: 18rem>
+            <div class="card d-flex flex-row " id="${arrayLista[i].nombre}" style="width: 18rem>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Nombre </li>
-                    <li class="list-group-item">${arrayLista[i].nombre}</li>
-                    <li class="list-group-item">Edad</li>
-                    <li class="list-group-item">${arrayLista[i].edad} </li>
-                    <li class="list-group-item">Telefono</li>
-                    <li class="list-group-item">${arrayLista[i].telefono} </li>
-                    <li class="list-group-item"> Precio </li>
-                    <li class="list-group-item"> ${arrayLista[i].tipoConsulta} </li>
-                    <li class="list-group-item"> Paciente Numero </li>
-                    <li class="list-group-item">${arrayLista[i].nroCliente} </li>
+                    <li class="list-group-item">Nombre: ${arrayLista[i].nombre}</li>
+                    <li class="list-group-item">Edad: ${arrayLista[i].edad}</li>
+                    <li class="list-group-item">Telefono: ${arrayLista[i].telefono}</li>
+                    <li class="list-group-item">Precio: ${arrayLista[i].tipoConsulta} </li>
+                    <li class="list-group-item">Paciente Numero: ${arrayLista[i].nroCliente}</li>
+                    <li class="list-group-item">Mail: ${arrayLista[i].mail}</li>
                     <button id="botonEliminar" onclick ="pacienteEliminado(${arrayLista[i].nroCliente})"> Eliminar Paciente </button>
                 </ul>
             </div>`;
@@ -81,14 +76,49 @@ function cargarPaciente() {
     let nombre = document.getElementById("nombre").value; 
     let edad = document.getElementById("edad").value;
     let telefono = document.getElementById("tel").value;
-    let tipoConsulta = document.getElementById("select").value;
+    let mail = document.getElementById("correo").value;
+    let consultaType = document.getElementById("select").value;
+    let consulta;
+    let emailConsulta;
+    switch(consultaType){
+        case '1': 
+            consulta = precio/2;
+            emailConsulta = "Primera Visita";
+            break;
+        case '2': 
+            consulta = precio;
+            emailConsulta = "Consulta Unica";
+            break;
+        case '3': 
+            consulta = precio*6;
+            emailConsulta = "Promo Mensual"; 
+            break;      
+    }
     if( nombre.length>0 && edad.length>0 && telefono.length>0) {
         pacienteAgregadoAlert();
-        let paciente = new Persona(nombre,edad,telefono,tipoConsulta,nroCliente++);
+        let paciente = new Persona(nombre,edad,telefono,consulta,nroCliente++,mail);
         pacientes.push(paciente);
         localStorage.removeItem("pacientes");
         localStorage.setItem("pacientes", JSON.stringify(pacientes));
         listaPacienteDOM (pacientes);
+        let params = {
+            user_id: 'EmsKI95M8poLOO417',
+            service_id: 'service_xncp93c',
+            template_id: 'template_cvxgrhv',
+            template_params: {
+              'name': nombre,
+              'email': mail,
+              'consultas': emailConsulta,
+            }
+        };
+        let headers = {
+            'Content-type': 'application/json'
+        };
+        fetch('https://api.emailjs.com/api/v1.0/email/send', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(params)
+        });    
     }
     else{
         errorCampo();
@@ -119,7 +149,7 @@ const precio = 1700;
 let pacientes = [];
 setPacientes();
 listaPacienteDOM (pacientes);
-let botonIngresar = document.getElementById ("botonIngresar");
+let botonIngresar = document.getElementById ("button");
 botonIngresar.addEventListener("click", cargarPaciente);
 let enter = document.getElementById("select");
 enter.addEventListener("keypress", cargarPaciente);
